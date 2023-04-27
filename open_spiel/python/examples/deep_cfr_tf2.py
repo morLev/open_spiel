de5e71e0596f2826ae3813ea6b6f66b5e4f35092
morLev <morlev92@gmail.com>
2023-04-27 13:24:12 +0300

    experiment

--- open_spiel/python/examples/deep_cfr_tf2.py
+++ open_spiel/python/examples/deep_cfr_tf2.py
@@ -13,63 +13,155 @@
 # limitations under the License.
 
 """Python Deep CFR example."""
+import datetime
+import os
+import pickle
 
 from absl import app
 from absl import flags
 from absl import logging
+import numpy as np
 
 from open_spiel.python import policy
 from open_spiel.python.algorithms import deep_cfr_tf2
 from open_spiel.python.algorithms import expected_game_score
 from open_spiel.python.algorithms import exploitability
+
 import pyspiel
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer("num_iterations", 100, "Number of iterations")
-flags.DEFINE_integer("num_traversals", 150, "Number of traversals/games")
-flags.DEFINE_string("game_name", "leduc_poker", "Name of the game")
+flags.DEFINE_integer("num_iterations", 1, "Number of iterations")
+flags.DEFINE_integer("num_traversals", 100, "Number of traversals/games")
+flags.DEFINE_string("game_name", "python_block_dominoes", "Name of the game")
 
+####### current ############3
 
 def main(unused_argv):
   logging.info("Loading %s", FLAGS.game_name)
   game = pyspiel.load_game(FLAGS.game_name)
+
+  time = datetime.datetime.now()
+  root_file = f"/home/mor/Desktop/DEV/open_spiel/open_spiel/algo_results/{time}"
+
+  save_strategy_memories = f"{root_file}/strategy"
+  save_advantage_networks = f"{root_file}/advantage"
+  save_lost = f"{root_file}/losses"
+
+  os.makedirs(save_strategy_memories)
+  os.makedirs(save_advantage_networks)
+  os.makedirs(save_lost)
+
+ # TODO: why they use adam istead of SGD:
+      #  . We perform 4,000 mini-batch stochastic gradient descent (SGD) iterations using a batch size of 10,000 and perform parameter
+      # updates using the Adam optimizer (Kingma & Ba, 2014)
+      # with a learning rate of 0.001, with gradient norm clipping
+      # to 1. For HULH we use 32,000 SGD iterations and a batch
+
+ # todo: dont the same net aritecture + our is why smaller
+ # todo: memory_capacity=4e7, int the article he saying 4e7 infosets for each player
+
   deep_cfr_solver = deep_cfr_tf2.DeepCFRSolver(
       game,
-      policy_network_layers=(64, 64, 64, 64),
-      advantage_network_layers=(64, 64, 64, 64),
+      policy_network_layers=(16, 16),
+      advantage_network_layers=(16, 16),
       num_iterations=FLAGS.num_iterations,
       num_traversals=FLAGS.num_traversals,
       learning_rate=1e-3,
-      batch_size_advantage=2048,
-      batch_size_strategy=2048,
-      memory_capacity=1e6,
-      policy_network_train_steps=5000,
-      advantage_network_train_steps=500,
+      batch_size_advantage=10,
+      batch_size_strategy=10,
+      memory_capacity=int(1e6),
+      policy_network_train_steps=200,
+      advantage_network_train_steps=200,
       reinitialize_advantage_networks=True,
+      save_strategy_memories = save_strategy_memories,
+      save_advantage_networks = save_advantage_networks,
       infer_device="cpu",
       train_device="cpu")
-  _, advantage_losses, policy_loss = deep_cfr_solver.solve()
-  for player, losses in advantage_losses.items():
-    logging.info("Advantage for player %d: %s", player,
-                 losses[:2] + ["..."] + losses[-2:])
-    logging.info("Advantage Buffer Size for player %s: '%s'", player,
-                 len(deep_cfr_solver.advantage_buffers[player]))
-  logging.info("Strategy Buffer Size: '%s'",
-               len(deep_cfr_solver.strategy_buffer))
-  logging.info("Final policy loss: '%s'", policy_loss)
 
-  average_policy = policy.tabular_policy_from_callable(
-      game, deep_cfr_solver.action_probabilities)
+  policies = []
+  for i in range(1):
+      policy_net, advantage_losses, policy_loss = deep_cfr_solver.solve()
+      deep_cfr_solver.save_policy_network(root_file + '/policy_network_iter' + str(i))
+      policies.append(policy_net)
 
-  conv = exploitability.nash_conv(game, average_policy)
-  logging.info("Deep CFR in '%s' - NashConv: %s", FLAGS.game_name, conv)
+      np.save(save_lost + f'/advantage_losses_{i}.npy', np.array(advantage_losses, dtype=object), allow_pickle=True)
+      np.save(save_lost + f'/policy_losses_{i}.npy', policy_loss, allow_pickle=True)
 
-  average_policy_values = expected_game_score.policy_value(
-      game.new_initial_state(), [average_policy] * 2)
-  print("Computed player 0 value: {}".format(average_policy_values[0]))
-  print("Computed player 1 value: {}".format(average_policy_values[1]))
 
+  avg_lst = []
+  for i in range(len(policies)):
+      if i == len(policies)- 1:
+          break
+      returns_list1 = simulate_games(game, [policies[i],policies[i+1]], 20)
+      returns_list2 = simulate_games(game, [policies[i+1],policies[i]], 20)
+
+      logging.info( f"returns of {i} against {i+1}:\n"+ str(returns_list1))
+      logging.info( f"returns of {i} against {i+1}:\n"+ str(returns_list2))
+      returns_list2_opp = [[ret[1],ret[0]] for ret in returns_list2]
+      returns=returns_list1 + returns_list2_opp
+      avg1=sum([ret[0] for ret in returns])/len(returns)
+      avg2=sum([ret[1] for ret in returns])/len(returns)
+      logging.info( f"avg of returns of {i} against {i+1}:\n"+ str([avg1,avg2]))
+      avg_lst.append([avg1, avg2])
+
+  with open(f"{root_file}/bots_by_iterations_result.pkl", "wb") as f:
+      pickle.dump(avg_lst, f)
+
+def simulate_games(game, agents, games_num):
+    returns_list = []
+    for _ in range(games_num):
+        returns_list.append(simulate(game, agents))
+
+    return returns_list
+
+def simulate(game, agents):
+    state = game.new_initial_state()
+    rng = np.random.RandomState(42)
+
+    # Print the initial state
+    logging.info("INITIAL STATE")
+    logging.info( str( state ) )
+
+    while not state.is_terminal():
+        # The state can be three different types: chance node,
+        # simultaneous node, or decision node
+        current_player = state.current_player()
+        if state.is_chance_node():
+            # Chance node: sample an outcome
+            outcomes = state.chance_outcomes()
+            num_actions = len( outcomes )
+            logging.info( "Chance node with " + str( num_actions ) + " outcomes" )
+            action_list, prob_list = zip( *outcomes )
+            action = rng.choice(action_list, p=prob_list)
+            logging.info( f"Sampled outcome: {state.action_to_string( state.current_player(), action )}")
+            state.apply_action( action )
+        else:
+            # Decision node: sample action for the single current player
+            legal_actions = state.legal_actions()
+            for action in legal_actions:
+                logging.info( "Legal action: {} ({})".format(
+                    state.action_to_string( current_player, action ), action ) )
+            nn = agents[current_player]
+            prob = deep_cfr_tf2.DeepCFRSolver.action_probabilities_s(nn, state)
+            action_list, prob_list = zip(*prob)
+
+            prob_list = [p/sum(prob_list) for p in prob_list]
+            action = np.random.choice(action_list, p=list(prob_list))
+
+            action_string = state.action_to_string( current_player, action )
+            logging.info("Player " + str(current_player) + ", chose action: " + action_string)
+            state.apply_action( action )
+
+        logging.info( "" )
+        logging.info( "NEXT STATE:" )
+        logging.info( str( state ) )
+
+    # Game is now done. logging.info utilities for each player
+    returns = state.returns()
+    for pid in range( game.num_players() ):
+        logging.info( "Utility for player {} is {}".format( pid, returns[pid] ) )
+    return returns
 
 if __name__ == "__main__":
   app.run(main)
