1847e80f8d06167237a59e476c321123abd52a25
mor <morlev92@gmail.com>
2023-05-16 10:59:12 +0300

    init mini games

--- open_spiel/python/games/__init__.py
+++ open_spiel/python/games/__init__.py
@@ -27,6 +27,9 @@ pyspiel.register_game(_GAME_TYPE, KuhnPokerGame)
 """
 
 from open_spiel.python.games import block_dominoes
+from open_spiel.python.games import block_dominoes_6
+from open_spiel.python.games import block_dominoes_10
+
 from open_spiel.python.games import dynamic_routing
 from open_spiel.python.games import iterated_prisoners_dilemma
 from open_spiel.python.games import kuhn_poker
